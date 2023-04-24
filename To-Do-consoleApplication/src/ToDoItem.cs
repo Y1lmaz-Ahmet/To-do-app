05df4e8f73a276b00ce311df5816c2d2e4a7b040
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-24 11:54:43 +0200

    Add project files.

---
+++ To-Do-consoleApplication/src/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_consoleApplication.src
+{
+    internal class ToDoItem
+    {
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
